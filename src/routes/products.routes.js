0408959dd2f4b731e01221cebff6a29fa48a7324
Breno Façanha <brenofacanha.ti@gmail.com>
2025-09-12 16:08:44 -0300

    adicionando as rotas dos products crud

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const productsController = require('../controllers/products.controller');
 
-router.get('/products', (req, res) => {
-    res.send('Lista de produtos');
-    console.log('rota de produtos funcionando');
-});
+router.get('/products', productsController.getAllProducts);
+router.get('/products/:id', productsController.getProductById);
+router.post('/products', productsController.createProduct);
+router.put('/products/:id', productsController.updateProduct);
+router.delete('/products/:id', productsController.deleteProduct);
 
 module.exports = router;
